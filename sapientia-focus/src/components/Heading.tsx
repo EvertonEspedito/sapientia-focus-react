77003ed959f6e0405e904d683a1e7a106b26f705
Everton Espedito <evertondev505@gmail.com>
2026-02-23 15:08:21 -0300

    adição

---
+++ sapientia-focus/src/components/Heading.tsx
@@ -0,0 +1,12 @@
+import './Heading.css';
+
+export function Heading() {
+    return (
+        <>
+            <header>
+                <h1 className="headTitle">Sapientia Focus</h1>
+            </header>
+            
+        </>
+    );
+}
